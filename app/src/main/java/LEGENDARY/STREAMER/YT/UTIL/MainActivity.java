bbd5832b81b5f3cf6f08ff2dfed0fccea54a8132
nnotOfficial <collegeklepu@gmail.com>
2022-08-25 10:45:49 +0530

    0.1.0

---
+++ app/src/main/java/LEGENDARY/STREAMER/YT/UTIL/MainActivity.java
@@ -0,0 +1,14 @@
+package LEGENDARY.STREAMER.YT.UTIL;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
